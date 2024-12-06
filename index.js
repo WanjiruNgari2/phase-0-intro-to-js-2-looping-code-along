eb0963bb5b90b7d300bcbdbab75f407126f6c4a5
Damaris Ngari <wanjirungari2@gmail.com>
2024-12-07 02:29:03 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+//  writeCards()
+const people = ["Guadalupe", "Ollie", "Aki"];
+const event = "surprise";
+
+function  writeCards(people,event) {
+    let messages = [];
+    for (let i = 0; i < people.length; i++) {
+    messages.push (`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages; 
+}
+console.log(writeCards(people,event));
+
+//countdown
+function countDown (number) {
+    for(let i = number; i >= 0; i--)
+      console.log(i);  
+}
